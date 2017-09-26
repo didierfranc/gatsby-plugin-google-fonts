fcda71a5928fb06120b0a34d1d04ae74168ac2fa
Didier Franc <didierfranc@me.com>
2017-09-26 11:47:33 +0200

    es5

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,20 +1,40 @@
-import React from 'react'
+'use strict'
 
-const format = string =>
-  string
+var _react = require('react')
+
+var _react2 = _interopRequireDefault(_react)
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+
+var format = function format(string) {
+  return string
     .split(' ')
-    .map(s => s.replace(/^\w/, s => s.toUpperCase()))
+    .map(function(s) {
+      return s.replace(/^\w/, function(s) {
+        return s.toUpperCase()
+      })
+    })
     .join(' ')
+}
 
-const getFonts = options =>
-  options.fonts
+var getFonts = function getFonts(options) {
+  return options.fonts
     .map(format)
     .join('|')
     .replace(/ /g, '+')
+}
 
-exports.onRenderBody = ({ setHeadComponents }, options) => {
-  const link = 'https://fonts.googleapis.com/css?family=' + getFonts(options)
+exports.onRenderBody = function(_ref, options) {
+  var setHeadComponents = _ref.setHeadComponents
+
+  var link = 'https://fonts.googleapis.com/css?family=' + getFonts(options)
   setHeadComponents([
-    React.createElement('link', { key: 'fonts', href: link, rel: 'stylesheet' })
+    _react2.default.createElement('link', {
+      key: 'fonts',
+      href: link,
+      rel: 'stylesheet'
+    })
   ])
 }
